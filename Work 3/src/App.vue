87de0d4a77d1fe04b6336ab9b8d369f4e1f84cae
Berkhan <123936730+nahkreb@users.noreply.github.com>
2024-06-30 15:52:26 +0300

    Work 3

---
+++ Work 3/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+<p v-bind:style="paragrafstil">Vivamus vehicula maximus lorem id porttitor. Fusce non accumsan lorem. Integer euismod placerat ligula nec congue. Nunc cursus magna vitae felis elementum, eu tristique nunc rutrum. Donec efficitur nibh enim, sed mollis mi imperdiet vel. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse risus sem, faucibus at gravida id, tincidunt sit amet diam. Vivamus faucibus est in quam sagittis, a suscipit ligula maximus. Aenean ultrices urna ex, a ultrices turpis gravida sed. Nunc semper ac lacus et faucibus.</p>
+<button @click="stildegistir">stil değiştir</button>
+</template>
+
+<script>
+export default {
+  name:"data-binding",
+  data(){
+    return{
+      paragrafstil:"font-size:20px; color:blue;"
+    };
+  },
+  methods:{
+    stildegistir(){
+      this.paragrafstil="font-size:50px; color:red;"
+    }
+  },
+};
+</script>
+
+<style>
+</style>
