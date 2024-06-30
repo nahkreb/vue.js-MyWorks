31d8bec2c013c44be3128771d039ec7bd9fd5d8e
Berkhan <123936730+nahkreb@users.noreply.github.com>
2024-06-30 15:49:11 +0300

    Work 2

---
+++ Work 2/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+<ul>
+  <li v-for="takim in takimlar" :key="takim">
+    <strong>Takım adı :</strong> {{ takim.takimAdi }} <br />
+    <strong>Kuruluş Yılı :</strong> {{ takim.KurulusYili }} 
+  </li>
+</ul>
+</template>
+
+<script>
+export default {
+  name: 'for-döngüsü',
+  data(){
+    return{
+      takimlar:[
+        {
+          takimAdi:"Fenerbahçe",
+          KurulusYili:"1907"
+        },
+        {
+          takimAdi:"Beşiktaş",
+          KurulusYili:"1903"
+        },
+        {
+          takimAdi:"Galatasaray",
+          KurulusYili:"1905"
+        },
+      ]
+    };
+  },
+};
+</script>
+
+<style>
+li{
+margin: 1em;
+font-size: 1.3em;
+}
+</style>
